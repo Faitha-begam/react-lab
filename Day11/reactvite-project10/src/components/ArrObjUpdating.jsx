4dc359236fa4e975e3a3e9ff03ebf8d97d8d0348
Faitha-begam <faithabegam078@gmail.com>
2026-07-17 01:41:17 +0530

    day 11 in react

--- Day11/reactvite-project10/src/components/ArrObjUpdating.jsx
+++ Day11/reactvite-project10/src/components/ArrObjUpdating.jsx
@@ -1,24 +1,42 @@
 import { useState } from "react"
 
 const ArrObjUpdating = () => {
-  const [arr,setArr] = useState([1,2,3,4,5])
-  const [obj,setObj] = useState({name:"faitha", age:21})
+  const [arr,setArr] = useState(["Vijay","Ajith","Suriya"])
+  const [obj,setObj] = useState({name:"faitha", course:"Js"})
+  const [office,setOffice] = useState({company:"Google", city:"Chennai"})
 
   const changeValue =()=>{
-    const copyArr = arr.map((e)=>e===2?[e,7]:e)
+    const copyArr = arr.map((e)=>e==="Ajith"?"SK":e)
     setArr(copyArr)
   }
   const changeObj =()=>{
-    setObj({name:"Begam"})
+    setObj({...obj,course:"React"})
+  }
+  const changeOffice =()=>{
+    setOffice({...office,company:"Microsoft"})
   }
   return (
     <>
-      <p className="text-center">{arr}</p>
+    <div className="flex justify-center gap-6 m-50">
+      <div className="bg-green-200 w-80 p-9 rounded-xl">
+      <h1 className="font-bold text-xl">Updating Array</h1> <br />
+      <p>{arr.join(", ")}</p> <br />
       <button className="bg-blue-300 rounded-lg p-3 w-30" onClick={changeValue}>click</button>
+      </div>
 
-      <div>
-        <p>{obj.name}</p>
+      <div className="bg-green-200 w-80 p-9 rounded-xl">
+        <h1 className="font-bold text-xl">Updating Object</h1> <br />
+        <p>{obj.name}</p> <br />
+        <p>{obj.course}</p> <br />
         <button className="bg-blue-300 rounded-lg p-3 w-30" onClick={changeObj}>Change Obj</button>
+      </div>
+
+      <div className="bg-green-200 w-80 p-9 rounded-xl">
+        <h1 className="font-bold text-xl">Updating Object</h1> <br />
+        <p>{office.company}</p> <br />
+        <p>{office.city}</p> <br />
+        <button className="bg-blue-300 rounded-lg p-3 w-30" onClick={changeOffice}>Change </button>
+      </div>
       </div>
     </>
   )
