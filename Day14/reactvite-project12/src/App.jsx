4420b2408b8475afc5b020baebd5b7b670590e00
Faitha-begam <faithabegam078@gmail.com>
2026-07-21 03:45:00 +0530

    day 14 in react

---
+++ Day14/reactvite-project12/src/App.jsx
@@ -0,0 +1,100 @@
+// import { useState } from "react";
+
+import { useState } from "react"
+
+// const App = () => {
+
+//   const [inputBox,setInputBox] = useState({userName:"", userEmail:"", userPassword:""})
+//   const [inputButton, setInputButton] = useState([])
+
+//   const inputValue=(e)=>{
+
+//    setInputBox({...inputBox,[e.target.name] : e.target.value})
+//     // console.log(`name: ${e.target.name} value: ${e.target.value}`)
+    
+//   }
+
+//   const submitForm=(e)=>{
+//       e.preventDefault()
+
+//       const inputArr = [...inputButton]
+//       inputArr.push(inputBox)
+//       setInputButton(inputArr)
+
+//       setInputBox({
+//         userName:"", userEmail:"", userPassword:""
+//       })
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={submitForm}>
+//         <input type="text" value={inputBox.userName} placeholder="Enter the Name" name="userName" onChange={inputValue}/>
+//         <input type="email" value={inputBox.userEmail} placeholder="Enter the email" name="userEmail" onChange={inputValue}/>
+//         <input type="password" value={inputBox.userPassword} placeholder="Enter the password" name="userPassword" onChange={inputValue}/>
+//         <input type="submit"/>
+//       </form>
+
+//       <div>
+//        {inputButton.map((inputBox, i) => (
+//          <div key={i}>
+//          <h2>{inputBox.userName}</h2>
+//          <h2>{inputBox.userEmail}</h2>
+//          <h2>{inputBox.userPassword}</h2>
+//          </div>
+//        ))}
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+const App = () => {
+  
+  const [data,setData] = useState("")
+  const [arr,setArr] = useState([])
+  const [edit,setEdit] = useState(null)
+
+  const handleChange=(e)=>{
+   setData(e.target.value)
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+
+    const newArr = [...arr]
+    const userData = {
+      id:Date.now(),
+      data
+    }
+    newArr.push(userData)
+    setArr(newArr)
+    setData("")
+  }
+
+  const editData=(e,i)=>{
+     setData(e.data)
+     setEdit(e.id)
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="enter name" value={data} onChange={handleChange} />
+        <input type="submit" value="Add" />
+      </form>
+      <div>
+        {arr.map((arr,i)=>(
+           <div key={arr.id}>
+
+           <p>{i+1}. {arr.data}</p>
+           <button onClick={()=>editData(arr,i)}>Edit Name</button>
+           </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
