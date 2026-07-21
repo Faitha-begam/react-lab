5b93e7c83aa1b16fffa761b46d85a23dd1df4f91
Faitha-begam <faithabegam078@gmail.com>
2026-07-21 11:58:56 +0530

    day 15 in react

---
+++ Day15/reactvite-project12/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const App = () => {
+
+  const fetchData = async()=>{
+  const [productData, setProductData] = useState([])
+
+  const getData = await fetch("https://dummyjson.com/products")
+  
+  const parseData = await getData.json()
+   console.log(parseData);
+  setProductData(parseData)
+}
+
+  return (
+    <>
+      {productData.map((e)=>(
+        <div key={e.id}>
+          <p>{e.title}</p>
+          <p>{e.price}</p>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default App
