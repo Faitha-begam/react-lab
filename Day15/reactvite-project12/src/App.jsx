0d8c3224e1ad7bb68ab9af9c3c0b4fb87eb6acac
Faitha-begam <faithabegam078@gmail.com>
2026-07-22 11:16:36 +0530

    Day 15 task, Day 16 project in react

--- Day15/reactvite-project12/src/App.jsx
+++ Day15/reactvite-project12/src/App.jsx
@@ -1,25 +1,36 @@
-import { useState } from "react"
+// import { useState } from "react"
+import UseEffectPractice from './components/UseEffectPractice'
+
+// const App = () => {
+
+//   const fetchData = async()=>{
+//   const [productData, setProductData] = useState([])
+
+//   const getData = await fetch("https://dummyjson.com/products")
+  
+//   const parseData = await getData.json()
+//    console.log(parseData);
+//   setProductData(parseData)
+// }
+
+//   return (
+//     <>
+//       {productData.map((e)=>(
+//         <div key={e.id}>
+//           <p>{e.title}</p>
+//           <p>{e.price}</p>
+//         </div>
+//       ))}
+//     </>
+//   )
+// }
+
+// export default App
 
 const App = () => {
-
-  const fetchData = async()=>{
-  const [productData, setProductData] = useState([])
-
-  const getData = await fetch("https://dummyjson.com/products")
-  
-  const parseData = await getData.json()
-   console.log(parseData);
-  setProductData(parseData)
-}
-
   return (
     <>
-      {productData.map((e)=>(
-        <div key={e.id}>
-          <p>{e.title}</p>
-          <p>{e.price}</p>
-        </div>
-      ))}
+      <UseEffectPractice/>
     </>
   )
 }
--- Day15/reactvite-project12/src/components/useEffectPractice.jsx
+++ Day15/reactvite-project12/src/components/useEffectPractice.jsx
@@ -1,23 +1,44 @@
 import { useState, useEffect } from "react"
 
+
+
+const UseEffectPractice = () => {
+
 const [count, setCount] = useState(0)
-const useEffectPractice = () => {
+const [name, setName] = useState("")
 
    useEffect(()=>{
        console.log('increased', count)
        
       },[count])
+
+const handleNameChange =(e)=>{
+   setName(e.target.value)
+}
+
+  useEffect(()=>(
+    console.log("Welcome: " + e.target.value)
+    
+  ),[name])
+
   return (
     
       <>
-      <h1>Count: {count}</h1>
+      <div>
+        <h1>Count: {count}</h1>
 
       <button onClick={() => setCount(count + 1)}>
         Increment
       </button>
+      </div>
+
+      <div>
+        <h1>Task 2 - Page Title Updater</h1>
+        <input type="text" value={name} placeholder="Enter User Name " onChange={handleNameChange} />
+      </div>
     </>
 
   )
 }
 
-export default useEffect
+export default UseEffectPractice
